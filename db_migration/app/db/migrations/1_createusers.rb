32bb3e47e171296f00bf1bbf00f7a68fc9c4aab3
ChoboBunny <kanin.dewit@gmail.com>
2017-11-28 16:52:32 +0100

    Updated the usermodel.

--- db_migration/app/db/migrations/1_createusers.rb
+++ db_migration/app/db/migrations/1_createusers.rb
@@ -1,14 +1,14 @@
 Sequel.migration do
   up do
     create_table(:users) do
-      primary_key :id
-      String :email, :null=>false, :unique=>true
-      String :password, :null=>false
-      String :username, :null=>false, :unique=>true
-      Boolean :validated_email, :null=>false
+      primary_key :ID
+      String :EMAIL, :null=>false, :unique=>true
+      String :PASSWORD, :null=>false
+      String :USERNAME, :null=>false, :unique=>true
+      Boolean :VALIDATED_EMAIL, :null=>false
     end
     alter_table(:users) do
-      set_column_default :validated_email, false
+      set_column_default :VALIDATED_EMAIL, false
     end
   end
   down do
--- src/main/scala/io/bunkitty/scifimmo/server/model/User.scala
+++ src/main/scala/io/bunkitty/scifimmo/server/model/User.scala
@@ -5,7 +5,7 @@ import slick.jdbc.PostgresProfile.api._
 import io.bunkitty.scifimmo.server.db.mappings.HashedPasswordMapping._
 import io.bunkitty.scifimmo.argon2.HashedPassword
 
-case class User(id: Option[Long], email: String, password: HashedPassword, displayName: String) {
+case class User(id: Option[Long], email: String, password: HashedPassword, displayName: String, validatedEmail: Boolean = false) {
 
 }
 
@@ -13,6 +13,7 @@ class Users(tag: Tag) extends Table[User](tag, "users") {
   def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
   def email = column[String]("EMAIL")
   def password = column[HashedPassword]("PASSWORD")
-  def displayName = column[String]("DISPLAY_NAME")
-  def * = (id.?, email, password, displayName) <> (User.tupled, User.unapply)
+  def username = column[String]("USERNAME")
+  def validatedEmail = column[Boolean]("VALIDATED_EMAIL")
+  def * = (id.?, email, password, username, validatedEmail) <> (User.tupled, User.unapply)
 }
