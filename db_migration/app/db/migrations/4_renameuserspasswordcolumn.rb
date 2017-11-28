fd2cc35399deb1384132dca611062c560958242d
Alexandra DeWit <alexa.dewit@gmail.com>
2017-11-28 17:29:04 +0100

    DB column name fixes

--- db_migration/app/db/migrations/4_renameuserspasswordcolumn.rb
+++ db_migration/app/db/migrations/4_renameuserspasswordcolumn.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
   up do
-    rename_column :users, :password, :password_digest
+    rename_column :users, :PASSWORD, :PASSWORD_DIGEST
   end
   down do
-    rename_column :users, :password_digest, :password
+    rename_column :users, :PASSWORD_DIGEST, :PASSWORD
   end
 end
