1095bf43f87814de7264b5ab68986417578a74cb
Alexandra DeWit <alexa.dewit@gmail.com>
2017-11-30 10:51:34 +0100

    Fix column order

--- db_migration/app/db/migrations/6_createcharacters.rb
+++ db_migration/app/db/migrations/6_createcharacters.rb
@@ -4,6 +4,7 @@ Sequel.migration do
       primary_key :ID, :null=>false, type: :Bignum
       foreign_key :FK_USER_ID, :USERS, :null=>false, type: :Bignum
       String      :NAME, :null=>false, :unique=>true
+      String      :SPECIES_NAME, :null => false, :default => "Human"
     end
   end
   down do
--- db_migration/app/db/migrations/8_add_basic_fields_to_chracter.rb
+++ db_migration/app/db/migrations/8_add_basic_fields_to_chracter.rb
@@ -6,10 +6,13 @@ Sequel.migration do
       add_column :LOCATION_Z, Integer
       add_column :CURRENT_HEALTH, Float
       add_column :CURRENT_TOUGHNESS, Float
+      add_column :CURRENT_CONSTITUTION, Float
       add_column :CURRENT_STAMINA, Float
       add_column :CURRENT_ENDURANCE, Float
+      add_column :CURRENT_QUICKNESS, Float
       add_column :CURRENT_FOCUS, Float
       add_column :CURRENT_CLARITY, Float
+      add_column :CURRENT_WILLPOWER, Float
     end
   end
   down do
@@ -18,11 +21,14 @@ Sequel.migration do
       drop_column :LOCATION_Y
       drop_column :LOCATION_Z
       drop_column :CURRENT_HEALTH
+      drop_column :CURRENT_CONSTITUTION
       drop_column :CURRENT_TOUGHNESS
       drop_column :CURRENT_STAMINA
       drop_column :CURRENT_ENDURANCE
+      drop_column :CURRENT_QUICKNESS
       drop_column :CURRENT_FOCUS
       drop_column :CURRENT_CLARITY
+      drop_column :CURRENT_WILLPOWER
     end
   end
 end
