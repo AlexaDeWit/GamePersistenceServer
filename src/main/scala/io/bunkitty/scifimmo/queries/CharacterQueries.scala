ad64472d6e3f9abc3b7e35830e8baa30f46735c1
Alexandra DeWit <alexa.dewit@gmail.com>
2018-01-31 19:24:00 +0100

    Cleanup

--- src/main/scala/io/bunkitty/scifimmo/queries/CharacterQueries.scala
+++ src/main/scala/io/bunkitty/scifimmo/queries/CharacterQueries.scala
@@ -7,6 +7,17 @@ import io.bunkitty.scifimmo.model.Character
 object CharacterQueries {
 
   def findCharacterQuery(id: Long): Query0[Character] =
-    sql"""SELECT * FROM "CHARACTERS" WHERE "ID" = $id""".query[Character]
+    sql"""SELECT
+             "ID", "FK_USER_ID",
+             "NAME","SPECIES_NAME",
+             "LOCATION_X","LOCATION_Y","LOCATION_Z",
+             "CURRENT_HEALTH","CURRENT_TOUGHNESS","CURRENT_CONSTITUTION",
+             "CURRENT_STAMINA","CURRENT_ENDURANCE","CURRENT_QUICKNESS",
+             "CURRENT_FOCUS","CURRENT_CLARITY", "CURRENT_WILLPOWER"
+         FROM "CHARACTERS"
+         WHERE "ID" = $id""".query[Character]
+
+  def findCharacter(id: Long): ConnectionIO[Option[Character]] = findCharacterQuery(id).option
+
 
 }
